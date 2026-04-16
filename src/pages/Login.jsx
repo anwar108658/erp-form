e915a2ccce54ae98f6a8ea895878cb011344836a
anwar108658 <anwar108658@gmail.com>
2026-04-16 18:11:08 +0500

    add some

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,26 @@
 import img from '../assets/logo.png'
 import logo from '../assets/tech.jpg'
 import '../App.css'
-import {Button, TextField} from '@mui/material'
+import {Box, Button, TextField} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
     const navigate = useNavigate()
   return (
     <div className='h-screen flex items-center justify-center'>
-        <div className='animate__animated animate__bounceInLeft w-full h-[80vh] max-w-280 grid grid-cols-12 bg-gray-100 '>
-            <div className='col-span-5'>
+        <div className='w-full h-[80vh] max-w-280 grid grid-cols-12 shadow-2xl bg-gra-100 text-gray-700'>
+            <div className='animate__animated animate__fadeInLeft col-span-5 flex flex-col'>
                 <figure className='p-2 bg-blue-950'>
                     <img draggable='false' className='w-[50%]' src={img} alt="" />
                 </figure>
-                <div>
-
-                </div>
+                <Box className='bg-gray-100 flex flex-col justify-center gap-5 px-5 flex-1'>
+                    <h1 className='text-2xl font-bold'>System Access</h1>
+                    <TextField id="filled-basic" label="User Name" variant="filled" />
+                    <TextField id="filled-basic" label="Password" variant="filled" />
+                    <Button variant='outlined' size='large' className='mt-5!'>Login</Button>
+                </Box>
             </div>
-            <figure className='col-span-7'>
+            <figure className='animate__animated animate__fadeInDown col-span-7'>
                 <img draggable='false' className='h-full w-full brightness-70  contrast-120' src={logo} alt="" />
             </figure>
         </div>
