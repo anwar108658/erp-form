85e7158bc6652e3534546e9e81ed35f62b572a72
anwar108658 <anwar108658@gmail.com>
2026-04-16 16:54:34 +0500

    add anim

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import img from '../assets/login.jpg'
+import img from '../assets/logo.png'
 import logo from '../assets/tech.jpg'
 import '../App.css'
 import {Button, TextField} from '@mui/material'
@@ -8,12 +8,17 @@ const Login = () => {
     const navigate = useNavigate()
   return (
     <div className='h-screen flex items-center justify-center'>
-        <div className='slideOutDown w-full h-[80vh] max-w-280 grid grid-cols-12 bg-gray-100 '>
+        <div className='animate__animated animate__bounceInLeft w-full h-[80vh] max-w-280 grid grid-cols-12 bg-gray-100 '>
             <div className='col-span-5'>
-                d
+                <figure className='p-2 bg-blue-950'>
+                    <img draggable='false' className='w-[50%]' src={img} alt="" />
+                </figure>
+                <div>
+
+                </div>
             </div>
             <figure className='col-span-7'>
-                <img className='h-full w-full brightness-90  contrast-120' src={logo} alt="" />
+                <img draggable='false' className='h-full w-full brightness-70  contrast-120' src={logo} alt="" />
             </figure>
         </div>
     </div>
