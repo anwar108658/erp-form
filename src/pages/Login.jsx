f62c8f390f435bf37a83c30744d571b2321505fa
anwar108658 <anwar108658@gmail.com>
2026-04-17 16:49:26 +0500

    add some

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import img from '../assets/logo.png'
-import logo from '../assets/tech.jpg'
+import logo from '../assets/logo.png'
+import login from '../assets/login5.png'
 import '../App.css'
 import {Box, Button, InputAdornment, TextField} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
@@ -9,10 +9,10 @@ const Login = () => {
     const navigate = useNavigate()
   return (
     <div className='h-screen flex items-center justify-center'>
-        <div className='w-full h-[80vh] max-w-280 grid grid-cols-12 shadow-2xl bg-gra-100 text-gray-700'>
-            <div className='animate__animated animate__fadeInLeft col-span-5 flex flex-col'>
+        <div className='w-full max-h-[89vh] h-[89vh]  max-w-280 flex bg-gray-200  shadow-2xl text-gray-700'>
+            <div className='animate__animated animate__fadeInLeft flex-4 flex flex-col'>
                 <figure className='p-2 bg-blue-950'>
-                    <img draggable='false' className='w-[50%]' src={img} alt="" />
+                    <img draggable='false' className='w-[50%]' src={logo} alt="" />
                 </figure>
                 <Box className='bg-gray-100 flex flex-col justify-center gap-5 px-5 flex-1'>
                     <h1 className='text-2xl font-bold'>System Access</h1>
@@ -27,8 +27,8 @@ const Login = () => {
                     <Button variant='outlined' size='large' className='mt-5!'>Login</Button>
                 </Box>
             </div>
-            <figure className='animate__animated animate__fadeInDown col-span-7'>
-                <img draggable='false' className='h-full w-full brightness-70  contrast-120' src={logo} alt="" />
+            <figure className='animate__animated animate__fadeInDown flex-5  w-full h-full'>
+                <img draggable='true' className='w-full h-full brightness-70  contrast-120' src={login} alt="" />
             </figure>
         </div>
     </div>
